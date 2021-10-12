2c979d023b781f95e01738a96baa0edc980a1bbc
zelek684 <73654594+zelek684@users.noreply.github.com>
2021-10-12 12:55:33 +0200

    CREATE

---
+++ ZADANIA1/main.py
@@ -0,0 +1,71 @@
+#zadanie1
+x='A'
+y='Lazoryszyn'
+
+def dane(dan1,dan2):
+    return dan1 +'.'+ dan2
+print (dane(x,y))
+print (dane('A','Lazoryszyn'))
+
+
+#zadanie2
+def dane(dan1, dan2):
+    return dan1.capitalize()+'.'+dan2.capitalize()
+print(dane("A", "Lazoryszyny"))
+
+#zadanie3
+def dane(rok1, rok2, wiek):
+    return int(rok1 + rok2) - wiek
+print(dane("20", "21", 23))
+
+#zadanie4
+def dane1(dan1,dan2):
+    return dan1.capitalize() + '.' + dan2.capitalize()
+def dane2(imie, nazwisko, funkcja):
+    return funkcja(imie, nazwisko) + " funkcja_z_zadania_2"
+print(dane2("A", "Lazoryszyn",dane1))
+
+#zadanie5
+def dane(dziel1, dziel2):
+    if dziel1 >=0 and dziel2 > 0 : return dziel1 / dziel2
+print(dane(6, 3))
+print(dane(15,0))
+
+#zadanie6
+
+suma = 0
+while(suma<100):
+    suma = suma + int(input())
+
+#zadanie7
+danex = [1,2,3]
+daney= ['Ang','La','zor']
+def daned(xxx):
+    return tuple(xxx)
+print(daned(danex))
+print(daned(daney))
+#zadanie8_faster
+def dane():
+    dan = []
+    for i in range(5):
+        dan.append(int(input()))
+    return tuple(dan)
+print(dane())
+#zadanie9
+def dane(dan):
+    tyg = {1: "poniediałek",2:"wtorek",3:"środa",4:"czwartek",5:"piątek",6:"sobota",7:"niedziela"}
+    return tyg[dan]
+print(dane(7))
+
+def dane10(slowo):
+    y=True
+    for x in range(len(slowo)):
+        if slowo[x]!=slowo[-x-1]:
+            y=False
+
+    return y
+
+print(dane10("Ang"))
+print(dane10("yn"))
+
+
